07bc5afc8af4ccb1c49f9570699cb0eca90efa26
Andrew Leap <andyleap@gmail.com>
2018-07-24 13:53:03 -0700

    Add better styling and some limited editing

--- boltinspect.go
+++ boltinspect.go
@@ -18,8 +18,6 @@ type BoltInspect struct {
 func New(db *bolt.DB) *BoltInspect {
 	tpls := template.New("inspect.tpl")
 	template.Must(tpls.Parse(inspectTemplate))
-	template.Must(tpls.New("bucket.tpl").Parse(bucketTemplate))
-	template.Must(tpls.New("item.tpl").Parse(itemTemplate))
 	template.Must(tpls.New("view.tpl").Parse(viewTemplate))
 
 	return &BoltInspect{
@@ -38,6 +36,17 @@ type itemData struct {
 }
 
 func (bi *BoltInspect) InspectEndpoint(rw http.ResponseWriter, req *http.Request) {
+	static := req.FormValue("static")
+	if static != "" {
+		switch static {
+		case "skeleton":
+			skeleton.ServeHTTP(rw, req)
+		case "normalize":
+			normalize.ServeHTTP(rw, req)
+		}
+		return
+	}
+
 	bucket := req.FormValue("bucket")
 	buckets := strings.Split(bucket, "/")
 	bucketsbyte := make([][]byte, 0)
@@ -46,6 +55,30 @@ func (bi *BoltInspect) InspectEndpoint(rw http.ResponseWriter, req *http.Request
 			bucketsbyte = append(bucketsbyte, []byte(v))
 		}
 	}
+
+	if req.Method == "POST" {
+		action := req.FormValue("action")
+		if action == "delete" {
+			bi.db.Update(func(tx *bolt.Tx) error {
+				if len(bucketsbyte) == 1 {
+					return tx.DeleteBucket(bucketsbyte[0])
+				}
+				bucket := tx.Bucket(bucketsbyte[0])
+				for _, b := range bucketsbyte[1 : len(bucketsbyte)-1] {
+					bucket = bucket.Bucket(b)
+				}
+				last := bucketsbyte[len(bucketsbyte)-1]
+				if bucket.Bucket(last) != nil {
+					return bucket.DeleteBucket(last)
+				}
+				return bucket.Delete(last)
+			})
+		}
+		path := strings.Join(buckets[:len(buckets)-1], "/")
+		http.Redirect(rw, req, "?bucket="+path, http.StatusSeeOther)
+		return
+	}
+
 	bucketdata := make([]*bucketData, 0)
 	itemdata := make([]*itemData, 0)
 	fmt.Println(bucketsbyte)
@@ -81,24 +114,15 @@ func (bi *BoltInspect) InspectEndpoint(rw http.ResponseWriter, req *http.Request
 			}
 		}
 		bucket.ForEach(func(k, v []byte) error {
-			if v == nil {
-				bucketdata = append(bucketdata, &bucketData{
-					ID: idPrefix + "/" + string(k),
-				})
-			} else {
-				itemdata = append(itemdata, &itemData{
-					ID:      idPrefix + "/" + string(k),
-					Content: string(v),
-				})
-			}
+			bucketdata = append(bucketdata, &bucketData{
+				ID: idPrefix + "/" + string(k),
+			})
 			return nil
 		})
 		bi.Templates.ExecuteTemplate(rw, "inspect.tpl", struct {
-			Buckets []*bucketData
-			Items   []*itemData
+			Items []*bucketData
 		}{
 			bucketdata,
-			itemdata,
 		})
 		return nil
 
@@ -107,26 +131,33 @@ func (bi *BoltInspect) InspectEndpoint(rw http.ResponseWriter, req *http.Request
 }
 
 var inspectTemplate = `
-<html><head></head><body>
-<ul>
-	<li><h1>Buckets</h1></li>
-	{{range .Buckets}}
-	<li>{{template "bucket.tpl" .}}</li>
-	{{end}}
-	<li><h1>Items</h1></li>
+<html><head>
+  <link rel="stylesheet" href="?static=normalize">
+  <link rel="stylesheet" href="?static=skeleton">
+</head><body>
+<div class="container">
+<table class="u-full-width">
+<thead>
+	<tr>
+		<th>Name</th>
+		<th>Actions</th>
+	</tr>
+</thead>
+<tbody>
 	{{range .Items}}
-	<li>{{template "item.tpl" .}}</li>
+	<tr>
+		<td><a href="?bucket={{.ID}}">{{.ID}}</a></td>
+		<td>
+			<form action="" method="POST">
+				<button class="button-primary" name="action" value="delete">Delete</button>
+			</form>
+		</td>
+	</tr>
 	{{end}}
-</ul>
+</tbody>
+</table>
+</div>
 </body></html>
-`
-
-var bucketTemplate = `
-<a href="?bucket={{.ID}}">{{.ID}}</a>
-`
-
-var itemTemplate = `
-<a href="?bucket={{.ID}}">{{.ID}}</a>
 `
 
 var viewTemplate = `
@@ -135,3 +166,7 @@ var viewTemplate = `
 {{.Content}}
 </body></html>
 `
+
+func css(a asset) http.Handler {
+	return a
+}
